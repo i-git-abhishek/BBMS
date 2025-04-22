b11f06dc147d8ebcb1dfa4744e8ab95daf3935bc
i-git-abhishek <ggolu1701@gmail.com>
2025-04-22 12:07:07 +0530

    set donors route in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/query', require('./routes/QueryRunner'));
+app.use('/api/donors', require('./routes/Donors'));
 
 app.get('/', (req, res) => {
     res.send('BBMS API Running');
