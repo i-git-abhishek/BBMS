19bd85f0f2ee1279e2df2f637feffe7f1cb4e1d9
i-git-abhishek <ggolu1701@gmail.com>
2025-04-27 15:43:48 +0530

    setting up for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const pool = require("./db");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use("/api/query", require("./routes/QueryRunner"));
 app.use("/api/donors", require("./routes/Donors"));
@@ -14,6 +16,9 @@ app.use("/api/bloodstock", require("./routes/BloodStock"));
 app.use("/api/register", require("./routes/Register"));
 app.get("/", (req, res) => {
   res.send("BBMS API Running");
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
