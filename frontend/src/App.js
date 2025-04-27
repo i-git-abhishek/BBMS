4e408717f07ea0555eabed6ede6c7ff1a1b45d48
i-git-abhishek <ggolu1701@gmail.com>
2025-04-27 16:10:33 +0530

    route correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import Home from './pages/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Router,{BrowserRouter, Route, Routes} from 'react-router-dom';
-import BloodStock from './pages/BloodStock';
-import Hospitals from './pages/Hospitals';
-import Requests from './pages/Requests';
-import Donors from './pages/Donors';
-import CustomQuery from './pages/CustomQuery';
-import AboutUs from './pages/AboutUs';
-import MoreInfo from './pages/MoreInfo';
-import './App.css';
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Router, { BrowserRouter, Route, Routes } from "react-router-dom";
+import BloodStock from "./pages/BloodStock";
+import Hospitals from "./pages/Hospitals";
+import Requests from "./pages/Requests";
+import Donors from "./pages/Donors";
+import CustomQuery from "./pages/CustomQuery";
+import AboutUs from "./pages/AboutUs";
+import MoreInfo from "./pages/MoreInfo";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element = {<Home/>} />
-        <Route path = "/BloodStock" element = {<BloodStock/>} />
-        <Route path = "/Hospitals" element = {<Hospitals/>} />
-        <Route path = "/Requests" element = {<Requests/>} />
-        <Route path = "/Donors" element = {<Donors/>} />
-        <Route path = "/CustomQuery" element = {<CustomQuery/>} />
-        <Route path = "/AboutUs" element = {<AboutUs/>} />
-        <Route path="/blood-group/:group" element={<MoreInfo />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/BloodStock" element={<BloodStock />} />
+        <Route path="/Hospitals" element={<Hospitals />} />
+        <Route path="/Requests" element={<Requests />} />
+        <Route path="/Donors" element={<Donors />} />
+        <Route path="/CustomQuery" element={<CustomQuery />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/blood-group/:bloodGroup" element={<MoreInfo />} />
       </Routes>
     </BrowserRouter>
   );
